9ff45dc5aa2fbe3d4c84d7b2856e5e447f317fc3
Laxmi Mewada <laxmimewada47@gmail.com>
2026-05-10 19:40:49 +0530

    Update Weather_Observation_Station_11.sql with comments
    Add comments and fix REGEXP condition for city selection

--- Basic Select/Weather_Observation_Station_11.sql
+++ Basic Select/Weather_Observation_Station_11.sql
@@ -1,1 +1,9 @@
+-- Problem: Weather Observation Station 11
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Concepts Used: REGEXP
 
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY NOT REGEXP '^[AEIOU]'
+OR CITY NOT REGEXP '[AEIOU]$';
