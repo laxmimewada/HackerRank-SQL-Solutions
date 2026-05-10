8bd37c1d146de8dfa22553102f1223b6c5679820
Laxmi Mewada <laxmimewada47@gmail.com>
2026-05-10 19:41:51 +0530

    Add SQL query for Weather Observation Station 12
    Add SQL query to select distinct cities not starting and ending with vowels.

--- Basic Select/Weather_Observation_Station_12.sql
+++ Basic Select/Weather_Observation_Station_12.sql
@@ -1,1 +1,8 @@
+-- Problem: Weather Observation Station 12
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Concepts Used: REGEXP
 
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY NOT REGEXP '^[AEIOU].*[AEIOU]$';
