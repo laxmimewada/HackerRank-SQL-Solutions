49ea2fd74b90654587d62259d897b1671eb03719
Laxmi Mewada <laxmimewada47@gmail.com>
2026-05-10 19:35:54 +0530

    Add query for cities starting and ending with vowels
    Add SQL query to select distinct cities starting and ending with vowels.

--- Basic Select/Weather_Observation_Station_8.sql
+++ Basic Select/Weather_Observation_Station_8.sql
@@ -1,1 +1,8 @@
+-- Problem: Weather Observation Station 8
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Concepts Used: REGEXP
 
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY REGEXP '^[AEIOU].*[AEIOU]$';
