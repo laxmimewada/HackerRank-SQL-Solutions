2475d32ea2872b8c8885d5448db2fae0aa707163
Laxmi Mewada <laxmimewada47@gmail.com>
2026-05-10 19:24:31 +0530

    Add SQL queries for occupations and their counts
    Added SQL queries to display names with occupations and count of each occupation.

--- Advanced Select/The_PADS.sql
+++ Advanced Select/The_PADS.sql
@@ -1,1 +1,13 @@
+-- Problem: The PADS
+-- Platform: HackerRank
+-- Difficulty: Medium
+-- Concepts Used: CONCAT, SUBSTRING, ORDER BY, GROUP BY
 
+SELECT CONCAT(Name, '(', LEFT(Occupation,1), ')')
+FROM OCCUPATIONS
+ORDER BY Name;
+
+SELECT CONCAT('There are a total of ', COUNT(Occupation), ' ', LOWER(Occupation), 's.')
+FROM OCCUPATIONS
+GROUP BY Occupation
+ORDER BY COUNT(Occupation), Occupation;
