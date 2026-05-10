854b6de2cc6b8e1107b218b2376ce89c67ecb72d
Laxmi Mewada <laxmimewada47@gmail.com>
2026-05-10 19:31:19 +0530

    Update Revising_the_Select_Query_I.sql

--- Basic Select/Revising_the_Select_Query_I.sql
+++ Basic Select/Revising_the_Select_Query_I.sql
@@ -1,1 +1,8 @@
+-- Problem: Revising the Select Query I
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Concepts Used: SELECT, WHERE
 
+SELECT *
+FROM CITY
+WHERE COUNTRYCODE = 'USA' AND POPULATION > 100000;
