e170a4cade576fba3049aa04b33872835d7d90e2
Laxmi Mewada <laxmimewada47@gmail.com>
2026-05-10 19:42:37 +0530

    Update Higher_Than_75_Marks.sql

--- Basic Select/Higher_Than_75_Marks.sql
+++ Basic Select/Higher_Than_75_Marks.sql
@@ -1,1 +1,9 @@
+-- Problem: Higher Than 75 Marks
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Concepts Used: ORDER BY, SUBSTRING
 
+SELECT NAME
+FROM STUDENTS
+WHERE MARKS > 75
+ORDER BY RIGHT(NAME,3), ID;
