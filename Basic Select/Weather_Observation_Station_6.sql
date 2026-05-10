6c5609a7e4aa78bfef69b83260b3b4dadc572e77
Laxmi Mewada <laxmimewada47@gmail.com>
2026-05-10 19:34:41 +0530

    Update Weather_Observation_Station_6.sql

--- Basic Select/Weather_Observation_Station_6.sql
+++ Basic Select/Weather_Observation_Station_6.sql
@@ -1,1 +1,8 @@
+-- Problem: Weather Observation Station 6
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Concepts Used: REGEXP
 
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY REGEXP '^[AEIOU]';
