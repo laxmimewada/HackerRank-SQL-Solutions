70692104dc84be42f374a2e1d236571e9cfff657
Laxmi Mewada <laxmimewada47@gmail.com>
2026-05-14 09:20:43 +0530

    Create SQL query for top earners
    Add SQL query to find top earners based on earnings calculation.

--- Aggregation/Top_Earners.sql
+++ Aggregation/Top_Earners.sql
@@ -1,1 +1,12 @@
+-- Problem: Top Earners
+-- Platform: HackerRank
+-- Difficulty: Medium
+-- Concepts Used: GROUP BY, ORDER BY
 
+SELECT
+    salary * months AS earnings,
+    COUNT(*)
+FROM Employee
+GROUP BY earnings
+ORDER BY earnings DESC
+LIMIT 1;
