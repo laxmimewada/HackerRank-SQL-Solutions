6eb696a5a8e443e51fdc9b1c7d86eaa8ebe304f5
Laxmi Mewada <laxmimewada47@gmail.com>
2026-05-14 09:26:12 +0530

    Implement query for Weather Observation Station 20
    Add SQL query to solve Weather Observation Station 20 problem.

--- Aggregation/Weather_Observation_Station_20.sql
+++ Aggregation/Weather_Observation_Station_20.sql
@@ -1,1 +1,14 @@
+-- Problem: Weather Observation Station 20
+-- Platform: HackerRank
+-- Difficulty: Hard
+-- Concepts Used: MEDIAN
 
+SELECT ROUND(LAT_N,4)
+FROM (
+    SELECT
+        LAT_N,
+        ROW_NUMBER() OVER(ORDER BY LAT_N) AS row_num,
+        COUNT(*) OVER() AS total_rows
+    FROM STATION
+) temp
+WHERE row_num = CEIL(total_rows/2);
