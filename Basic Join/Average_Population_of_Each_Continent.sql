87e279c65a799b0648ae534ac2e3e8e8f1ee3b29
Laxmi Mewada <laxmimewada47@gmail.com>
2026-05-14 09:28:05 +0530

    Update Average_Population_of_Each_Continent.sql

--- Basic Join/Average_Population_of_Each_Continent.sql
+++ Basic Join/Average_Population_of_Each_Continent.sql
@@ -1,1 +1,11 @@
+-- Problem: Average Population of Each Continent
+-- Platform: HackerRank
+-- Difficulty: Medium
+-- Concepts Used: JOIN, AVG, GROUP BY
 
+SELECT COUNTRY.CONTINENT,
+       FLOOR(AVG(CITY.POPULATION))
+FROM CITY
+JOIN COUNTRY
+ON CITY.COUNTRYCODE = COUNTRY.CODE
+GROUP BY COUNTRY.CONTINENT;
