548524dbaed2d7cf9f345379b347ae01e55c418b
Laxmi Mewada <laxmimewada47@gmail.com>
2026-05-14 09:31:16 +0530

    Update SQL_Project_Planning.sql

--- Advanced Join/SQL_Project_Planning.sql
+++ Advanced Join/SQL_Project_Planning.sql
@@ -1,1 +1,26 @@
+-- Problem: SQL Project Planning
+-- Platform: HackerRank
+-- Difficulty: Medium
+-- Concepts Used: DATEDIFF, GROUP BY
 
+SELECT Start_Date,
+       MIN(End_Date)
+FROM (
+    SELECT Start_Date
+    FROM Projects
+    WHERE Start_Date NOT IN (
+        SELECT End_Date
+        FROM Projects
+    )
+) s,
+(
+    SELECT End_Date
+    FROM Projects
+    WHERE End_Date NOT IN (
+        SELECT Start_Date
+        FROM Projects
+    )
+) e
+WHERE Start_Date < End_Date
+GROUP BY Start_Date
+ORDER BY DATEDIFF(MIN(End_Date), Start_Date), Start_Date;
