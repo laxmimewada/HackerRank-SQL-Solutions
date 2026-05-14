30909a6092412f3efa4114562e9d6fcf8ebd355e
Laxmi Mewada <laxmimewada47@gmail.com>
2026-05-14 09:21:53 +0530

    Update Weather_Observation_Station_14.sql

--- Aggregation/Weather_Observation_Station_14.sql
+++ Aggregation/Weather_Observation_Station_14.sql
@@ -1,1 +1,8 @@
+-- Problem: Weather Observation Station 14
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Concepts Used: MAX, ROUND
 
+SELECT ROUND(MAX(LAT_N),4)
+FROM STATION
+WHERE LAT_N < 137.2345;
