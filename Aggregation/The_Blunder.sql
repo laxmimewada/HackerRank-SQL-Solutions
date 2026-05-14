1d84c779be5c25d5a650d42788f4765e5cc4be73
Laxmi Mewada <laxmimewada47@gmail.com>
2026-05-14 09:20:20 +0530

    Insert SQL query for average salary calculation
    Add SQL query to calculate adjusted average salary

--- Aggregation/The_Blunder.sql
+++ Aggregation/The_Blunder.sql
@@ -1,1 +1,9 @@
+-- Problem: The Blunder
+-- Platform: HackerRank
+-- Difficulty: Medium
+-- Concepts Used: AVG, REPLACE, CEIL
 
+SELECT CEIL(
+    AVG(SALARY) - AVG(REPLACE(SALARY, 0, ''))
+)
+FROM EMPLOYEES;
