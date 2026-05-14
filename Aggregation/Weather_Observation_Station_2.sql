6eca9000dd4de5cfc6c78d8782ef751cefd9776b
Laxmi Mewada <laxmimewada47@gmail.com>
2026-05-14 09:21:09 +0530

    Update Weather_Observation_Station_2.sql

--- Aggregation/Weather_Observation_Station_2.sql
+++ Aggregation/Weather_Observation_Station_2.sql
@@ -1,1 +1,9 @@
+-- Problem: Weather Observation Station 2
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Concepts Used: ROUND, SUM
 
+SELECT
+    ROUND(SUM(LAT_N),2),
+    ROUND(SUM(LONG_W),2)
+FROM STATION;
