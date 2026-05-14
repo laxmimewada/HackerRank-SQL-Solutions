5a30bd5dc5b85462483b718fa45ae793c76132b8
Laxmi Mewada <laxmimewada47@gmail.com>
2026-05-14 09:27:42 +0530

    Add query for African cities from HackerRank
    Add SQL query to select city names from African countries.

--- Basic Join/African_Cities.sql
+++ Basic Join/African_Cities.sql
@@ -1,1 +1,10 @@
+-- Problem: African Cities
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Concepts Used: JOIN
 
+SELECT CITY.NAME
+FROM CITY
+JOIN COUNTRY
+ON CITY.COUNTRYCODE = COUNTRY.CODE
+WHERE COUNTRY.CONTINENT = 'Africa';
