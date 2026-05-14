058bdf7ec7c4140334486ec67ffc205a3e3b71e1
Laxmi Mewada <laxmimewada47@gmail.com>
2026-05-14 09:18:39 +0530

    Add SQL query for average population in California
    Added SQL query to calculate average population in California.

--- Aggregation/Revising_Aggregations_Averages.sql
+++ Aggregation/Revising_Aggregations_Averages.sql
@@ -1,1 +1,8 @@
+-- Problem: Revising Aggregations - Averages
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Concepts Used: AVG
 
+SELECT AVG(POPULATION)
+FROM CITY
+WHERE DISTRICT = 'California';
