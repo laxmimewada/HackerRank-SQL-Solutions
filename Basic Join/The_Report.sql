db59d1971aacd302558793e379fff32cc0473e98
Laxmi Mewada <laxmimewada47@gmail.com>
2026-05-14 09:28:22 +0530

    Create SQL report for student grades
    Added SQL query for generating a report based on student grades.

--- Basic Join/The_Report.sql
+++ Basic Join/The_Report.sql
@@ -1,1 +1,16 @@
+-- Problem: The Report
+-- Platform: HackerRank
+-- Difficulty: Medium
+-- Concepts Used: JOIN, CASE
 
+SELECT
+    CASE
+        WHEN Grades.GRADE < 8 THEN NULL
+        ELSE Students.NAME
+    END,
+    Grades.GRADE,
+    Students.MARKS
+FROM Students
+JOIN Grades
+ON Students.MARKS BETWEEN Grades.MIN_MARK AND Grades.MAX_MARK
+ORDER BY Grades.GRADE DESC, Students.NAME, Students.MARKS;
