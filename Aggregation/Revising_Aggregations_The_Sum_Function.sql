c2a3518a2ff3f27a06504a36ff48a42e6be4f0c7
Laxmi Mewada <laxmimewada47@gmail.com>
2026-05-14 09:18:09 +0530

    Update Revising_Aggregations_The_Sum_Function.sql

--- Aggregation/Revising_Aggregations_The_Sum_Function.sql
+++ Aggregation/Revising_Aggregations_The_Sum_Function.sql
@@ -1,1 +1,8 @@
+-- Problem: Revising Aggregations - The Sum Function
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Concepts Used: SUM
 
+SELECT SUM(POPULATION)
+FROM CITY
+WHERE DISTRICT = 'California';
