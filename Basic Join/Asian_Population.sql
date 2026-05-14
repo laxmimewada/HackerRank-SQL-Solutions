db7b6e4a1adda8880f14ecb48b4f5f4689ae821c
Laxmi Mewada <laxmimewada47@gmail.com>
2026-05-14 09:27:13 +0530

    Update Asian_Population.sql

--- Basic Join/Asian_Population.sql
+++ Basic Join/Asian_Population.sql
@@ -1,1 +1,10 @@
+-- Problem: Asian Population
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Concepts Used: JOIN, SUM
 
+SELECT SUM(CITY.POPULATION)
+FROM CITY
+JOIN COUNTRY
+ON CITY.COUNTRYCODE = COUNTRY.CODE
+WHERE COUNTRY.CONTINENT = 'Asia';
