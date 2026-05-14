6ae81b2ffa84ce3a9d698c90dcf15fb1e52cab41
Laxmi Mewada <laxmimewada47@gmail.com>
2026-05-14 09:22:18 +0530

    Create SQL query for Weather Observation Station 15
    Add SQL query for Weather Observation Station 15 problem.

--- Aggregation/Weather_Observation_Station_15.sql
+++ Aggregation/Weather_Observation_Station_15.sql
@@ -1,1 +1,10 @@
+-- Problem: Weather Observation Station 15
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Concepts Used: ROUND, ORDER BY
 
+SELECT ROUND(LONG_W,4)
+FROM STATION
+WHERE LAT_N < 137.2345
+ORDER BY LAT_N DESC
+LIMIT 1;
