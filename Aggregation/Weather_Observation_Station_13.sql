b805cd189d8c32a66c428a174b3699fa5e16bdfb
Laxmi Mewada <laxmimewada47@gmail.com>
2026-05-14 09:21:29 +0530

    Update Weather_Observation_Station_13.sql

--- Aggregation/Weather_Observation_Station_13.sql
+++ Aggregation/Weather_Observation_Station_13.sql
@@ -1,1 +1,9 @@
+-- Problem: Weather Observation Station 13
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Concepts Used: SUM, ROUND
 
+SELECT ROUND(SUM(LAT_N),4)
+FROM STATION
+WHERE LAT_N > 38.7880
+AND LAT_N < 137.2345;
