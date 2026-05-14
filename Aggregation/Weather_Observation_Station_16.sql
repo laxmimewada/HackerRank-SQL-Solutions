617ada726e82742bace9f50a475141116ba6bb46
Laxmi Mewada <laxmimewada47@gmail.com>
2026-05-14 09:23:00 +0530

    Add query for Weather Observation Station 16
    Add SQL query to find minimum latitude rounded to four decimal places.

--- Aggregation/Weather_Observation_Station_16.sql
+++ Aggregation/Weather_Observation_Station_16.sql
@@ -1,1 +1,8 @@
+-- Problem: Weather Observation Station 16
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Concepts Used: MIN, ROUND
 
+SELECT ROUND(MIN(LAT_N),4)
+FROM STATION
+WHERE LAT_N > 38.7780;
