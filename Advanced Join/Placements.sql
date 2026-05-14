fbd004649e332690fa460d58566c86829c727bdb
Laxmi Mewada <laxmimewada47@gmail.com>
2026-05-14 09:31:35 +0530

    Implement query for placements based on salary
    Add SQL query to find students with higher friend salaries

--- Advanced Join/Placements.sql
+++ Advanced Join/Placements.sql
@@ -1,1 +1,15 @@
+-- Problem: Placements
+-- Platform: HackerRank
+-- Difficulty: Medium
+-- Concepts Used: JOIN, ORDER BY
 
+SELECT s.name
+FROM Students s
+JOIN Friends f
+ON s.id = f.id
+JOIN Packages p1
+ON s.id = p1.id
+JOIN Packages p2
+ON f.friend_id = p2.id
+WHERE p2.salary > p1.salary
+ORDER BY p2.salary;
