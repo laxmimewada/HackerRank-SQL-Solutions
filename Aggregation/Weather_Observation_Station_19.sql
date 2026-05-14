fa9e3f6fa4eef5d3b98b2c69ce635dd33e37c037
Laxmi Mewada <laxmimewada47@gmail.com>
2026-05-14 09:24:13 +0530

    Add query for Weather Observation Station 19
    Add SQL query to calculate the distance between latitude and longitude.

--- Aggregation/Weather_Observation_Station_19.sql
+++ Aggregation/Weather_Observation_Station_19.sql
@@ -1,1 +1,12 @@
+-- Problem: Weather Observation Station 19
+-- Platform: HackerRank
+-- Difficulty: Medium
+-- Concepts Used: SQRT, POWER
 
+SELECT ROUND(
+    SQRT(
+        POWER(MAX(LAT_N) - MIN(LAT_N),2) +
+        POWER(MAX(LONG_W) - MIN(LONG_W),2)
+    ),
+4)
+FROM STATION;
