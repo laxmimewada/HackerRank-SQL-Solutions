3dfcc8900b62f73a0b1ff5a3341223c8f80e00a4
Laxmi Mewada <laxmimewada47@gmail.com>
2026-05-14 09:17:51 +0530

    Update Revising_Aggregations_The_Count_Function.sql

--- Aggregation/Revising_Aggregations_The_Count_Function.sql
+++ Aggregation/Revising_Aggregations_The_Count_Function.sql
@@ -1,1 +1,8 @@
+-- Problem: Revising Aggregations - The Count Function
+-- Platform: HackerRank
+-- Difficulty: Easy
+-- Concepts Used: COUNT
 
+SELECT COUNT(NAME)
+FROM CITY
+WHERE POPULATION > 100000;
